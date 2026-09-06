ee1f4152b435380647ed1252767dcbb3824528ec
jiazafa <javariazafar1234@gmail.com>
2026-09-06 04:08:23 -0700

    home lab3 task1
    Added a C program to demonstrate data types and their sizes.

---
+++ home-lab03/task1_datatypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+int age=20;
+  float height=5.6f;
+double pi=3.1415926;
+char grade ='a';
+bool is student = true;
+printf("int value:%d,size:%zu.bytes\n",age,size of(age));
+  printf("float value:%2f,size:5zu bytes\n",height,size of(height));
+printf("double value :%.14f,size:5zu bytes\n",pi,size of (pi));
+printf("char value :%c,size:%zu bytes\n",grade,sizeof(grade));
+printf("bool value:%d,size:%zu bytes\n",is student,
+  size of(is student));
+return 0;
+}
