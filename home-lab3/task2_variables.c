39c21896e93547027fe1087e11369a95a1bd8bad
jiazafa <javariazafar1234@gmail.com>
2026-09-06 04:20:35 -0700

    home-task3

---
+++ home-lab3/task2_variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int age=20;
+float height=2.0f;
+char grade='a'
+char name[]="javaria";
+long population=1000000L;
+printf("name:%s |age:%d | height:%.2f| grade:%c| population:%ld\n",
+  name,age,height,grade,population);
+return 0;
+}
